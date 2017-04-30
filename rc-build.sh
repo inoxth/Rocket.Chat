157cd0335ffb90f8164e5a777fbacf418a914f20
Somsak Sriprayoonsakul <somsaks@gmail.com>
2017-04-30 14:35:06 +0700

    Update build script according to Travis

--- rc-build.sh
+++ rc-build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -x
 
 SERVER_URL="https://rocketchat.inox.co.th"
-TARGET="/var/www/rocket.chat"
+TARGET="$HOME/rocket.chat-bundle"
 
 export NODE_ENV="production"
 
-meteor npm install --save bcrypt
-meteor add rocketchat:internal-hubot meteorhacks:kadira
-meteor build --server "$SERVER_URL" --directory "$TARGET"
+meteor npm install --save bcrypt && \
+	meteor npm install -g node-gyp node-pre-gyp && \
+	meteor add rocketchat:internal-hubot meteorhacks:kadira && \
+	meteor npm install && \
+	meteor build --headless "$TARGET"
+
+#	meteor build --server "$SERVER_URL" --directory "$TARGET"
