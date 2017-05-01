15b082a10c93e93835940572747de0770b275799
Somsak Sriprayoonsakul <somsaks@gmail.com>
2017-05-01 14:37:39 +0700

    Remove refernece to specific host url

--- rc-build.sh
+++ rc-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-SERVER_URL="https://rocketchat.inox.co.th"
+SERVER_URL="https://demo.rocket.chat"
 TARGET="$HOME/rocket.chat-bundle"
 
 export NODE_ENV="production"
@@ -9,7 +9,7 @@ export NODE_ENV="production"
 meteor npm install --save bcrypt && \
 	meteor npm install -g node-gyp node-pre-gyp && \
 	meteor npm install && \
-	meteor add rocketchat:internal-hubot meteorhacks:kadira && \
 	meteor build --headless "$TARGET"
 
+#	meteor add rocketchat:internal-hubot meteorhacks:kadira && \
 #	meteor build --server "$SERVER_URL" --directory "$TARGET"
