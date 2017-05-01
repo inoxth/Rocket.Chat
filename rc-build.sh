d4ee670b723caa5ca9503dd0a2ab125a378026b7
Somsak Sriprayoonsakul <somsaks@gmail.com>
2017-05-01 14:35:47 +0700

    Use instructions from .travis.yml instead

--- rc-build.sh
+++ rc-build.sh
@@ -8,8 +8,8 @@ export NODE_ENV="production"
 
 meteor npm install --save bcrypt && \
 	meteor npm install -g node-gyp node-pre-gyp && \
-	meteor add rocketchat:internal-hubot meteorhacks:kadira && \
 	meteor npm install && \
+	meteor add rocketchat:internal-hubot meteorhacks:kadira && \
 	meteor build --headless "$TARGET"
 
 #	meteor build --server "$SERVER_URL" --directory "$TARGET"
